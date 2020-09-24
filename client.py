4d5e1bab4fa61e09242df9f3edb96906bca01be1
Henrique Igai Wang <64105985+henrique-wang@users.noreply.github.com>
2020-09-24 00:28:56 -0300

    Etapa Luna
    Integrada a parte que representa a Luna no Client.py

--- client.py
+++ client.py
@@ -2,12 +2,23 @@
 import pyttsx3
 import importlib
 recognizer = importlib.import_module("Codigo_Guigs.3_Recognizer")
+employee_db = importlib.import_module("db_access_offline.db_employee", ".")
+comment_db = importlib.import_module("db_access_offline.db_comment", ".")
+comment_class = importlib.import_module("classes.comment_class", ".")
 
 def main():
     engine = pyttsx3.init()
     # Call recognizer and get employee name
-    #employeeName = recognizer.Recognize()
-    employeeName = "Luna"
-    engine.say('Good morning, {}'.format(employeeName))
+    idusers, idsmile = recognizer.Recognize()
+    userName = employee_db.getEmployeePerID(idusers[0])
+    engine.say('Boa noite, {}! Qual é a sua sugestão?'.format(userName))
+    message = "Message Luna"
+    area = "Area Luna"
+    smile = 0
+    if len(idsmile) > 0:
+        if idsmile[0] == idusers[0]:
+            smile = 1
+
+    comment_db.addComment(idusers[0], message, area, smile)
     engine.runAndWait()
 main()
